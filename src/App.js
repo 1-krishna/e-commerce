36d82fb46a9ed5d277ee758af3cc5672801d57b8
Krishna <krishnacaresu@gmail.com>
2020-04-04 23:12:51 +0530

    logged-in user can't navigate to sign-in page and redirected to homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
@@ -51,15 +51,27 @@ class App extends React.Component {
         <Switch>
           <Route path='/' exact component={HomePage} />
           <Route path='/shop' exact component={Shop} />
-          <Route path='/signin' exact component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ?
+                (<Redirect to='/' />) :
+                (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps
+  , mapDispatchToProps
+)(App);
