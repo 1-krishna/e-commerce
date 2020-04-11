71dfa7553e043967db996cf224785701ef91f172
Krishna <krishnacaresu@gmail.com>
2020-04-11 15:26:21 +0530

    moved shop data and directory data into reducer and created collection-overview component to make it easy and clean while creating further routes in shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 import './shop.styles.scss';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-class Shop extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherProps }) => (
-                    <CollectionPreview key={id} {...otherProps} />
-                ))}
-            </div>
-        );
-    }
+const Shop = () => {
+    return (
+        <div className='shop-page'>
+            <CollectionOverview />
+        </div>
+    );
 }
 
 export default Shop;
